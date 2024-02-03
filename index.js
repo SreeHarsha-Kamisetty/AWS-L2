25ed60731b7c5a0f562437286a591cb3f23461e0
SreeHarsha-Kamisetty <sreeharsha.kamisetty99@gmail.com>
2024-02-04 00:35:56 +0530

    testing

--- index.js
+++ index.js
@@ -1,73 +1,74 @@
-const express = require("express")
-require('dotenv').config();
-
-const path = require("path")
-const app = express();
+const express = require('express')
+const app = express()
 const AWS = require("aws-sdk");
+require('dotenv').config()
 const s3 = new AWS.S3()
-const bodyParser = require("body-parser")
-const multer = require("multer")
-// const storage = multer.diskStorage({
-//     destination: function (req, file, cb) {
-//       cb(null, "/tmp")
-//     },
-//     filename: function (req, file, cb) {
-//       const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-//       cb(null, file.originalname)
-//     }
-//   })
-const storage = multer.memoryStorage();
-  const upload = multer({ storage: storage })
+const bodyParser = require('body-parser');
 
-// app.get("/",(req,res)=>{
-//     res.send("home")
-// })
+app.use(bodyParser.json())
 
-// app.post('/profile', upload.single('avatar'), function (req, res, next) {
-   
-//     res.send("image uploaded")
-//   })
-  app.post('*', upload.single('avatar'), async (req, res) => {
-    let filename = req.path.slice(1);
-    console.log(filename);
-    console.log(req.file.buffer);
-
-    try {
-        await s3.putObject({
-            Body: req.file.buffer,
-            Bucket: "cyclic-rich-gray-coypu-kit-eu-west-3",
-            Key: filename,
-        }).promise();
-        res.set('Content-type', req.file.mimetype);
-        res.send('ok').end();
-    } catch (error) {
-        console.error('Error uploading to S3:', error);
-        res.status(500).send('Internal Server Error').end();
-    }
-});     
-
+// curl -i https://some-app.cyclic.app/myFile.txt
 app.get('*', async (req,res) => {
-    let filename = req.path.slice(1)
-  
-    try {
-      let s3File = await s3.getObject({
-        Bucket:  "cyclic-rich-gray-coypu-kit-eu-west-3",
-        Key: filename,
-      }).promise()
-  
-      res.set('Content-type', s3File.ContentType)
-      res.send(s3File.Body.toString()).end()
-    } catch (error) {
-      if (error.code === 'NoSuchKey') {
-        console.log(`No such key ${filename}`)
-        res.sendStatus(404).end()
-      } else {
-        console.log(error)
-        res.sendStatus(500).end()
-      }
+  let filename = req.path.slice(1)
+
+  try {
+    let s3File = await s3.getObject({
+      Bucket: "cyclic-rich-gray-coypu-kit-eu-west-3",
+      Key: filename,
+    }).promise()
+
+    res.set('Content-type', s3File.ContentType)
+    res.send(s3File.Body.toString()).end()
+  } catch (error) {
+    if (error.code === 'NoSuchKey') {
+      console.log(`No such key ${filename}`)
+      res.sendStatus(404).end()
+    } else {
+      console.log(error)
+      res.sendStatus(500).end()
     }
-  })
-const PORT = process.env.PORT || 8080;
-app.listen(PORT,()=>{
-    console.log(`Server running at http://localhost:${PORT}`)
+  }
+})
+
+
+// curl -i -XPUT --data '{"k1":"value 1", "k2": "value 2"}' -H 'Content-type: application/json' http://localhost:3000/myFile.txt
+app.put('*', async (req,res) => {
+  let filename = req.path.slice(1)
+
+  console.log(typeof req.body)
+
+  await s3.putObject({
+    Body: JSON.stringify(req.body),
+    Bucket: "cyclic-rich-gray-coypu-kit-eu-west-3",
+    Key: filename,
+  }).promise()
+
+  res.set('Content-type', 'text/plain')
+  res.send('ok').end()
+})
+
+// curl -i -XDELETE https://some-app.cyclic.app/myFile.txt
+app.delete('*', async (req,res) => {
+  let filename = req.path.slice(1)
+
+  await s3.deleteObject({
+    Bucket: "cyclic-rich-gray-coypu-kit-eu-west-3",
+    Key: filename,
+  }).promise()
+
+  res.set('Content-type', 'text/plain')
+  res.send('ok').end()
+})
+
+// /////////////////////////////////////////////////////////////////////////////
+// Catch all handler for all other request.
+app.use('*', (req,res) => {
+  res.sendStatus(404).end()
+})
+
+// /////////////////////////////////////////////////////////////////////////////
+// Start the server
+const port = process.env.PORT || 3000
+app.listen(port, () => {
+  console.log(`index.js listening at http://localhost:${port}`)
 })
