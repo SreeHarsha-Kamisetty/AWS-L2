7da44b8728d014cb5776c2a9b2e2ba964364d952
SreeHarsha-Kamisetty <sreeharsha.kamisetty99@gmail.com>
2024-02-03 22:15:00 +0530

    s3 bucket

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express")
-
+require('dotenv').config();
 const app = express();
-
+const AWS = require("aws-sdk");
+const s3 = new AWS.S3()
+const bodyParser = require("body-parser")
 const multer = require("multer")
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
@@ -23,7 +25,20 @@ app.post('/profile', upload.single('avatar'), function (req, res, next) {
    
     res.send("image uploaded")
   })
-
+app.put('*', async (req,res) => {
+    let filename = req.path.slice(1)
+  
+    console.log(typeof req.body)
+  
+    await s3.putObject({
+      Body: JSON.stringify(req.body),
+      Bucket: process.env.BUCKET,
+      Key: filename,
+    }).promise()
+  
+    res.set('Content-type', 'text/plain')
+    res.send('ok').end()
+  })
 
 app.listen(8080,()=>{
     console.log("Server running at http://localhost:8080")
